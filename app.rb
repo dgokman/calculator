e493af14e804c02a398059b776bfdf6fe2f83ae9
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-14 14:54:10 -0400

    all operators working with each other

--- app.rb
+++ app.rb
@@ -60,7 +60,7 @@ post '/divide' do
   numbers = []
   Number.all.each do |number|
     numbers << number.number
-    @number = numbers.join.to_i
+    @number = numbers.join.to_f
   end
   @operators = Operator.create(value: @number, category: "divide")
   @operators.save
@@ -72,40 +72,29 @@ post '/equal' do
   numbers = []
   Number.all.each do |number|
     numbers << number.number
-    @equal = numbers.join.to_i
+    @equal = numbers.join.to_f
   end
   @operators = Operator.create(value: @equal, category: "value")
   @operators.save
   Number.delete_all(category: "number")
-  if Operator.all.first.category == "plus"
-    @result = Operator.first.value
-    counter = 1
-    while counter < Operator.all.length
-     @result += Operator.offset(counter).limit(1).first.value
+  counter = 0
+  @result = Operator.first.value
+  while counter < (Operator.all.length - 1)
+    if Operator.offset(counter).limit(1).first.category == "plus"
+     @result += Operator.offset(counter + 1).limit(1).first.value.to_f
      counter += 1
-    end
-  elsif Operator.all.first.category == "minus"
-    @result = Operator.first.value
-    counter = 1
-    while counter < Operator.all.length
-     @result -= Operator.offset(counter).limit(1).first.value
+    elsif Operator.offset(counter).limit(1).first.category == "minus"
+     @result -= Operator.offset(counter + 1).limit(1).first.value.to_f
      counter += 1
-    end
-  elsif Operator.all.first.category == "times"
-    @result = Operator.first.value
-    counter = 1
-    while counter < Operator.all.length
-     @result *= Operator.offset(counter).limit(1).first.value
+    elsif Operator.offset(counter).limit(1).first.category == "times"
+     @result *= Operator.offset(counter + 1).limit(1).first.value.to_f
      counter += 1
-    end
-  else
-    @result = Operator.first.value
-    counter = 1
-    while counter < Operator.all.length
-     @result /= Operator.offset(counter).limit(1).first.value
+    else
+     @result /= Operator.offset(counter + 1).limit(1).first.value.to_f
      counter += 1
     end
   end
+
   Operator.destroy_all
   erb :index
 end
