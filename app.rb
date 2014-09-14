201c31039b0f50480a2e70ccefdcd8df39047af9
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-14 12:47:12 -0400

    all operators working

--- app.rb
+++ app.rb
@@ -24,24 +24,89 @@ post '/plus' do
   numbers = []
   Number.all.each do |number|
     numbers << number.number
-    @plus = numbers.join.to_i
+    @number = numbers.join.to_i
   end
-  @plus
-  @operators = Operator.create(value: @plus, category: "value")
+  @operators = Operator.create(value: @number, category: "plus")
+  @operators.save
+  Number.delete_all(category: "number")
+  erb :index
+end
+
+post '/minus' do
+  numbers = []
+  Number.all.each do |number|
+    numbers << number.number
+    @number = numbers.join.to_i
+  end
+  @operators = Operator.create(value: @number, category: "minus")
+  @operators.save
+  Number.delete_all(category: "number")
+  erb :index
+end
+
+post '/times' do
+  numbers = []
+  Number.all.each do |number|
+    numbers << number.number
+    @number = numbers.join.to_i
+  end
+  @operators = Operator.create(value: @number, category: "times")
+  @operators.save
+  Number.delete_all(category: "number")
+  erb :index
+end
+
+post '/divide' do
+  numbers = []
+  Number.all.each do |number|
+    numbers << number.number
+    @number = numbers.join.to_i
+  end
+  @operators = Operator.create(value: @number, category: "divide")
   @operators.save
   Number.delete_all(category: "number")
   erb :index
 end
 
 post '/equal' do
-  counter = 0
-  @result = 0
-  while counter < Operator.all.length
-    @result += Operator.order("id DESC").offset(counter).limit(1).first.value
-    counter += 1
+  numbers = []
+  Number.all.each do |number|
+    numbers << number.number
+    @equal = numbers.join.to_i
   end
-  @result
-  Operator.delete_all(category: "value")
+  @operators = Operator.create(value: @equal, category: "value")
+  @operators.save
+  Number.delete_all(category: "number")
+  if Operator.all.first.category == "plus"
+    @result = Operator.first.value
+    counter = 1
+    while counter < Operator.all.length
+     @result += Operator.offset(counter).limit(1).first.value
+     counter += 1
+    end
+  elsif Operator.all.first.category == "minus"
+    @result = Operator.first.value
+    counter = 1
+    while counter < Operator.all.length
+     @result -= Operator.offset(counter).limit(1).first.value
+     counter += 1
+    end
+  elsif Operator.all.first.category == "times"
+    @result = Operator.first.value
+    counter = 1
+    while counter < Operator.all.length
+     @result *= Operator.offset(counter).limit(1).first.value
+     counter += 1
+    end
+  else
+    @result = Operator.first.value
+    counter = 1
+    while counter < Operator.all.length
+     @result /= Operator.offset(counter).limit(1).first.value
+     counter += 1
+    end
+  end
+  Operator.destroy_all
   erb :index
 end
 
